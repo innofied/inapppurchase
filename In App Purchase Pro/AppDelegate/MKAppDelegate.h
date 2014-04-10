3c23ec0a8e3648757aee356f98b41156beea6081
milankamilya <milan.kamilya@innofied.com>
2014-04-10 12:39:06 +0530

    initial push

---
+++ In App Purchase Pro/AppDelegate/MKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MKAppDelegate.h
+//  In App Purchase
+//
+//  Created by Sandip Saha on 09/04/14.
+//  Copyright (c) 2014 innofied. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

d37d4f307e51b72fafabf531b2a519ecb8f2eb6e
milankamilya <milan.kamilya@innofied.com>
2014-04-10 13:04:30 +0530

    initial push

---
+++ In App Purchase/In App Purchase/MKViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKViewController.h
+//  In App Purchase
+//
+//  Created by Sandip Saha on 10/04/14.
+//  Copyright (c) 2014 innofied. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKViewController : UIViewController
+
+@end
---
+++ In App Purchase/In App Purchase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  In App Purchase
+//
+//  Created by Sandip Saha on 10/04/14.
+//  Copyright (c) 2014 innofied. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
